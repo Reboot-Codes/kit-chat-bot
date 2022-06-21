6791e72373d3ff24589876bdbd17878528559e53
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2022-06-21 14:34:21 -0700

    Added "bad word" filter.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import tmi from 'tmi.js';
 import chalk from 'chalk';
+import axios from 'axios';
 
 const channels = [
   'kitboga'
 ];
 
-function checker(value) {
-  var prohibited = [];
+console.log(`${chalk.blue('[INFO]')} Bot Starting...`);
 
+let prohibited = []
+const badWordList = 'http://www.bannedwordlist.com/lists/swearWords.txt';
+await axios.get(badWordList).then((res) => {
+  if (res.status == 200) {
+    console.log(`${chalk.green('[SUCCESS]')} Fetched Bad Word List!`);
+    res.data.split('\n').forEach((line) => { prohibited.push(line); });
+  } else {
+    console.error(`${chalk.red('[ERROR]')} Could not get bad words list from ${chalk.underline(badWordList)}!`);
+  }
+}).catch((err) => {
+  console.error(`${chalk.red('[ERROR]')} Could not get bad words list from ${chalk.underline(badWordList)}!`);
+  console.error(err);
+});
+
+function checker(value) {
   for (var i = 0; i < prohibited.length; i++) {
     if (value.toLowerCase().indexOf(prohibited[i].toLowerCase()) > -1) {
       return false;
@@ -16,8 +31,6 @@ function checker(value) {
   return true;
 }
 
-console.log(`${chalk.blue('[INFO]')} Bot Starting...`);
-
 const TMI_OPTIONS = {
   channels
 }
@@ -25,15 +38,16 @@ const TMI_OPTIONS = {
 const client = new tmi.Client(TMI_OPTIONS);
 
 client.on('connected', (address, port) => {
-  console.log(`${chalk.green('[SUCCESS]')} Connected to ${chalk.yellow(`${address}:${port}`)}!`)
+  console.log(`${chalk.green('[SUCCESS]')} Connected to ${chalk.yellow(`${address}:${port}`)}!`);
+  console.log(`${chalk.blue('[INFO]')} Listening to the following channels: ${chalk.cyan(`${channels.join(', ')}`)}`);
 });
 
 client.on('message', (channel, userState, message) => {
   if (channels.includes(channel)) {
     if (checker(message)) {
-      console.log(`${chalk.yellow('[MESSAGE]')} ${chalk.gray(userState.username)}: "${message}"`);
+      console.log(`${chalk.yellow('[MESSAGE]')} ${chalk.cyan(channel)}:${chalk.gray(`@${userState.username}`)}: ${chalk.bold(message)}`);
     } else {
-      console.log(`${chalk.yellow('[MESSAGE]')} ${chalk.gray(userState.username)}: ${chalk.red('FILTERED')}`);
+      console.log(`${chalk.yellow('[MESSAGE]')} ${chalk.cyan(channel)}:${chalk.gray(`@${userState.username}`)}: ${chalk.red('(filtered)')} ${chalk.bold(message)}`);
     }
   }
 })
